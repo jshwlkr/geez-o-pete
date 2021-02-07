f0906f309fdd61db776ee892da9862c51fff9845
Joshua Walker <joshua@snowandbones.com>
2021-02-07 02:23:58 -0500

    trying to fix gh-pages

--- .eleventy.js
+++ .eleventy.js
@@ -50,7 +50,7 @@ module.exports = function (eleventyConfig) {
 		return {
 				dir: {
 						input: 'src',
-						output: 'docs',
+						output: 'dist',
 						includes: '_includes',
 						layouts: '_layouts',
 						data: '_data'
